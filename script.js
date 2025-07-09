9d96661da6d9a8f4098a645e28f36c4e6b01d965
Jr <118298017+Jrgth91@users.noreply.github.com>
2025-07-08 21:02:18 -0400

    add basic game functionality

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log('Hello World')
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    const number = Math.random();
+    if (number < 0.33) {
+        return "Rock"
+    } else if (number > .33 && number < .66) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+function getHumanChoice() {
+    const choice = prompt("Rock, Paper or Scissor?")
+    return choice;
+
+}
+
+function playRound(humanChoice, computerChoice){
+    //get comp score, ask human for input, compare inputs but make it make less complex, list winner, give point to winner
+
+if (humanChoice === computerChoice) {
+    console.log(`Its A Tie`)
+    console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
+} else if (humanChoice === "Rock" && computerChoice !== "Paper" || humanChoice === "Paper" && computerChoice !== "Scissors" || humanChoice === "Scissors" && computerChoice !== "Rock") {
+    console.log(`You Win! ${humanChoice} Beats ${computerChoice}!`)
+    humanScore++
+    console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
+} else {
+    console.log(`You Lose! ${humanChoice} Loses to ${computerChoice}`) 
+        computerScore++
+    console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
+}
+}
+
+function playGame() {
+    while (humanScore < 3 && computerScore < 3) {
+    playRound(getHumanChoice(), getComputerChoice());
+}
+if (humanScore > computerScore) {
+    console.log("Congratulations! YOU WIN THE GAME!")
+} else { 
+
+    console.log("Sorry, You lose the game.")
+}
+
+}
+playGame();
+
+
