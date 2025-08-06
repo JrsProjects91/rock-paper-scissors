3ece319a8d91f07b0c150df0217b6991e2d64e95
JrsProjects91 <118298017+JrsProjects91@users.noreply.github.com>
2025-08-06 14:13:13 -0400

    add simple gui with displayable notifications

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let humanScore = 0;
 let computerScore = 0;
 
+//Define DOM Elements
+const human_score_dom = document.querySelector('#human_score')
+const computer_score_dom = document.querySelector('#computer_score')
+const notificationUpdates = document.querySelector('#notifications')
+const choiceInput = document.querySelector('#choice')
+const playButton = document.querySelector('button')
+
+
+const isGameOver = () => {
+    if ((computerScore + humanScore) < 5) {
+        return false
+    } else {
+        return true
+    }
+}
+
 function getComputerChoice() {
     const number = Math.random();
     if (number < 0.33) {
@@ -12,41 +28,65 @@ function getComputerChoice() {
     }
 }
 
+
+playButton.addEventListener("click", playGame)
+
 function getHumanChoice() {
-    const choice = prompt("Rock, Paper or Scissor?")
+    const choice = choiceInput.value
+    choiceInput.value = "";
     return choice;
 
+}
+
+function updateDomScores() {
+    human_score_dom.textContent = humanScore
+    computer_score_dom.textContent = computerScore
 }
 
 function playRound(humanChoice, computerChoice){
     //get comp score, ask human for input, compare inputs but make it make less complex, list winner, give point to winner
 
 if (humanChoice === computerChoice) {
-    console.log(`Its A Tie`)
-    console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
-} else if (humanChoice === "Rock" && computerChoice !== "Paper" || humanChoice === "Paper" && computerChoice !== "Scissors" || humanChoice === "Scissors" && computerChoice !== "Rock") {
-    console.log(`You Win! ${humanChoice} Beats ${computerChoice}!`)
+    notificationUpdates.textContent = `Its A Tie`;
+    computerScore++
     humanScore++
-    console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
+ 
+    
+} else if (humanChoice === "Rock" && computerChoice !== "Paper" || humanChoice === "Paper" && computerChoice !== "Scissors" || humanChoice === "Scissors" && computerChoice !== "Rock") {
+     notificationUpdates.textContent = `You Win This Round! "${humanChoice}" Beats ${computerChoice}!`;
+    humanScore++
+
 } else {
-    console.log(`You Lose! ${humanChoice} Loses to ${computerChoice}`) 
-        computerScore++
-    console.log(`Your Score: ${humanScore} Computer Score: ${computerScore}`)
+     notificationUpdates.textContent = `You Lose This Round. "${humanChoice}" Loses to ${computerChoice}`;
+    computerScore++
 }
 }
+
 
 function playGame() {
-    while (humanScore < 3 && computerScore < 3) {
-    playRound(getHumanChoice(), getComputerChoice());
+    if (!isGameOver()){
+             playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (isGameOver()) {
+        if (humanScore > computerScore) {
+             notificationUpdates.textContent = 'Congratulations! YOU WIN THE GAME!';
+            } else { 
+            notificationUpdates.textContent = "Sorry, You lose the game.";
 }
-if (humanScore > computerScore) {
-    console.log("Congratulations! YOU WIN THE GAME!")
-} else { 
-
-    console.log("Sorry, You lose the game.")
-}
+    }
+    updateDomScores()
 
 }
-playGame();
 
 
+updateDomScores()
+
+// click button 
+
+    //play game { check player scores}
+    // get and store computer score
+    // get player score 
+            // if player score is higher give 1 point to player
+            // update gui and norifications, clear player choice input
+            
